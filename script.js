914b192cc7777589fdb1be4bdb1da4d2712e5924
Guilherme Keslley <guilhermekeslley123@gmail.com>
2022-10-15 22:30:19 -0300

    Adicionando accordionList

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 import validityAndFilterRadios from "./js/filterRadios.js";
+import AccordionList from "./js/global/accordionList.js";
 
 validityAndFilterRadios();
+
+const accordion = new AccordionList("dt");
+accordion.init();
