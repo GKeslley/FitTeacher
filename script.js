513c8592a2c97a9e75e20fd5cf02775ffc95e694
Guilherme Keslley <guilhermekeslley123@gmail.com>
2022-10-20 23:14:39 -0300

    Adicionando o changeBackground

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import ChangeBackground from "./js/changeBackground.js";
 import validityAndFilterRadios from "./js/filterRadios.js";
 import AccordionList from "./js/global/accordionList.js";
 import Sidebar from "./js/global/sidebar.js";
@@ -9,3 +10,6 @@ accordion.init();
 
 const sidebar = new Sidebar(".sidebar-element", ".header");
 sidebar.init();
+
+const changeBackground = new ChangeBackground(".option-input");
+changeBackground.init();
